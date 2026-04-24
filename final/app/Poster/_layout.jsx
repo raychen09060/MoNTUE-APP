c9ce505cc500955f129378feb1d18d7340244164
ray <raychen09060@gmail.com>
2026-04-24 15:37:46 +0800

    init upload

--- final/app/Poster/_layout.jsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from 'expo-router';
-
-export default function PosterLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="[id]" options={{ title: 'Poster Details' }} />
-        </Stack>
-    );
-}
