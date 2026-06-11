aff688a87be712c6c42c5a37ef7aafdf61ebc2a2
reneehsieh3 <reixie2030@gmail.com>
2026-06-11 20:03:51 +0800

    TICKET

--- final/components/UserData.js
+++ final/components/UserData.js
@@ -3,6 +3,10 @@ export const UserData = [
         id: "",
         tel: "0900000000",
         ticket: [],
-        cart: [],
+        cart: {
+            A:0,
+            B:0,
+            C:0,
+        },
     }
 ]
